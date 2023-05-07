f45a6ab8d42041a0e5f1947d877144674b9a8344
Josh Noel <obsidize@gmail.com>
2023-05-07 10:15:45 -0500

    add basic test for ios advertising data parsing

--- tests/advertisement-decoder.spec.ts
+++ tests/advertisement-decoder.spec.ts
@@ -1,8 +1,43 @@
-import { AdvertisementDecoder } from '../src';
+import { Advertisement, AdvertisementDecoder } from '../src';
 
 describe('AdvertisementDecoder', () => {
 
 	it('can be created with no arguments', () => {
 		expect(new AdvertisementDecoder()).toBeTruthy();
-	})
+	});
+
+	it('normalizes iOS advertising data', () => {
+
+		const decoder = new AdvertisementDecoder();
+
+		const iosAdvertisingData = {
+			name: "demo",
+			id: "15B4F1C5-C9C0-4441-BD9F-1A7ED8F7A365",
+			advertising: {
+				kCBAdvDataLocalName: "demo",
+				kCBAdvDataManufacturerData: Uint8Array.of(0x00).buffer,
+				kCBAdvDataServiceUUIDs: [
+					"721b"
+				],
+				kCBAdvDataIsConnectable: true,
+				kCBAdvDataServiceData: {
+					"BBB0": Uint8Array.of(0x00).buffer,
+				},
+			},
+			rssi: -61
+		};
+
+		const decoded = decoder.decode(iosAdvertisingData);
+
+		const expectedOutput: Advertisement = {
+			...iosAdvertisingData,
+			advDataLocalName: iosAdvertisingData.advertising.kCBAdvDataLocalName,
+			advDataIsConnectable: iosAdvertisingData.advertising.kCBAdvDataIsConnectable,
+			advDataServiceData: iosAdvertisingData.advertising.kCBAdvDataServiceData,
+			advDataManufacturerData: iosAdvertisingData.advertising.kCBAdvDataManufacturerData,
+			advDataServiceUUIDs: iosAdvertisingData.advertising.kCBAdvDataServiceUUIDs
+		};
+
+		expect(decoded).toEqual(expectedOutput);
+	});
 });
